8eb65073e8c72dcc9520327f6541ad40f94a5564
Claire Dechaux <clairedechaux91@yahoo.fr>
2026-05-18 13:51:58 +0200

    Correction import

--- pipelines/report.py
+++ pipelines/report.py
@@ -18,7 +18,7 @@ from __future__ import annotations
 
 from datetime import datetime
 from pathlib import Path
-from typing import Any
+from typing import Any, Callable
 
 import polars as pl
 from tqdm import tqdm
